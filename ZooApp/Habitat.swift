8878024a255ae1b814082275ef39de244b28e605
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-12 16:11:06 -0400

    setting up Habitat + Pen structs

---
+++ ZooApp/Habitat.swift
@@ -0,0 +1,14 @@
+//
+//  Habitat.swift
+//  ZooApp
+//
+//  Created by DetroitLabs on 9/12/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+struct Habitat {
+    var pens: [Pen]
+    var name: String
+}
---
+++ ZooApp/Pen.swift
@@ -0,0 +1,14 @@
+//
+//  Pen.swift
+//  ZooApp
+//
+//  Created by DetroitLabs on 9/12/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+struct Pen {
+    var name: String
+    var animals: [Animal]
+}
