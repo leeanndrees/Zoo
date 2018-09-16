679a270f015433b60c1992885047e6e881d425a4
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-16 16:48:29 -0400

    replacing species strings w/ enum rawValue

--- ZooApp/TemporaryData.swift
+++ ZooApp/TemporaryData.swift
@@ -9,52 +9,53 @@
 import Foundation
 
 var animalsInZoo: [Animal] = [
-    Animal(species: "Lion", name: "Selma", sex: "F"),
-    Animal(species: "Lion", name: "Lionel", sex: "M"),
-    BabyAnimal(species: "Lion", name: "Fiona", sex: "F", age: "3 months"),
-    Animal(species: "Elephant", name: "Margot", sex: "F"),
-    Animal(species: "Elephant", name: "George", sex: "M"),
-    BabyAnimal(species: "Elephant", name: "Susan", sex: "F", age: "6 months"),
-    Animal(species: "Rhinoceros", name: "Carolyn", sex: "F"),
-    Animal(species: "Rhinoceros", name: "Richard", sex: "M"),
-    BabyAnimal(species: "Rhinoceros", name: "Bobby", sex: "M", age: "6 months"),
-    Animal(species: "Arctic Fox", name: "Meryl", sex: "F"),
-    Animal(species: "Polar Bear", name: "Poppy", sex: "F"),
-    Animal(species: "Wolf", name: "William", sex: "M")
+    Animal(species: Species.lion.rawValue, name: "Selma", sex: "F"),
+    Animal(species: Species.lion.rawValue, name: "Lionel", sex: "M"),
+    BabyAnimal(species: Species.lion.rawValue, name: "Fiona", sex: "F", age: "3 months"),
+    Animal(species: Species.elephant.rawValue, name: "Margot", sex: "F"),
+    Animal(species: Species.elephant.rawValue, name: "George", sex: "M"),
+    BabyAnimal(species: Species.elephant.rawValue, name: "Susan", sex: "F", age: "6 months"),
+    Animal(species: Species.rhino.rawValue, name: "Carolyn", sex: "F"),
+    Animal(species: Species.rhino.rawValue, name: "Richard", sex: "M"),
+    BabyAnimal(species: Species.rhino.rawValue, name: "Bobby", sex: "M", age: "6 months"),
+    Animal(species: Species.arcticFox.rawValue, name: "Meryl", sex: "F"),
+    Animal(species: Species.polarBear.rawValue, name: "Poppy", sex: "F"),
+    Animal(species: Species.wolf.rawValue, name: "William", sex: "M")
 ]
 
 var habitatsInZoo: [Habitat] = [
     grassland, arctic
 ]
 
-var lionPen = Pen(name: "Lion Pen", animals: [], animalSpecies: "Lion")
-var arcticFoxPen = Pen(name: "Arctic Fox Pen", animals: [], animalSpecies: "Arctic Fox")
+var grassland = Habitat(pens: [lionPen, elephantPen, rhinoPen], name: "Grassland")
+var arctic = Habitat(pens: [arcticFoxPen, polarBearPen, wolfPen], name: "Arctic")
+
+
+var lionPen = Pen(name: "Lion Pen", animals: [], animalSpecies: Species.lion.rawValue)
+var arcticFoxPen = Pen(name: "Arctic Fox Pen", animals: [], animalSpecies: Species.arcticFox.rawValue)
 var elephantPen = Pen(name: "Elephant Pen", animals: [], animalSpecies: "Elephant")
-var rhinoPen = Pen(name: "Rhinoceros Pen", animals: [], animalSpecies: "Rhinoceros")
-var polarBearPen = Pen(name: "Polar Bear Pen", animals: [], animalSpecies: "Polar Bear")
-var wolfPen = Pen(name: "Wolf Pen", animals: [], animalSpecies: "Wolf")
+var rhinoPen = Pen(name: "Rhinoceros Pen", animals: [], animalSpecies: Species.rhino.rawValue)
+var polarBearPen = Pen(name: "Polar Bear Pen", animals: [], animalSpecies: Species.polarBear.rawValue)
+var wolfPen = Pen(name: "Wolf Pen", animals: [], animalSpecies: Species.wolf.rawValue)
 
 var pensInZoo: [Pen] = [
     lionPen, arcticFoxPen, elephantPen, rhinoPen, polarBearPen, wolfPen
 ]
 
-var grassland = Habitat(pens: [lionPen, elephantPen, rhinoPen], name: "Grassland")
-var arctic = Habitat(pens: [arcticFoxPen, polarBearPen, wolfPen], name: "Arctic")
-
 func populatePens() {
     for animal in animalsInZoo {
         switch animal.species {
-        case "Lion":
+        case Species.lion.rawValue:
             lionPen.animals.append(animal)
-        case "Arctic Fox":
+        case Species.arcticFox.rawValue:
             arcticFoxPen.animals.append(animal)
-        case "Elephant":
+        case Species.elephant.rawValue:
             elephantPen.animals.append(animal)
-        case "Rhinoceros":
+        case Species.rhino.rawValue:
             rhinoPen.animals.append(animal)
-        case "Polar Bear":
+        case Species.polarBear.rawValue:
             polarBearPen.animals.append(animal)
-        case "Wolf":
+        case Species.wolf.rawValue:
             wolfPen.animals.append(animal)
         default:
             return
