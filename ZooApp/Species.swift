fa4b8184f1eae3a7f8cd14c66ae8c71e0af64de0
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-16 16:44:19 -0400

    adding enum for species

---
+++ ZooApp/Species.swift
@@ -0,0 +1,18 @@
+//
+//  Species.swift
+//  ZooApp
+//
+//  Created by DetroitLabs on 9/16/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+enum Species: String {
+    case lion = "Lion"
+    case arcticFox = "Arctic Fox"
+    case elephant = "Elephant"
+    case rhino = "Rhinoceros"
+    case polarBear = "Polar Bear"
+    case wolf = "Wolf"
+}
